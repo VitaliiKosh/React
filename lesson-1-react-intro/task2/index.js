32ef35770a40f4e51a76c95ef9e198a5b24b738a
Vitaly Koshlyak <vitaly.koshlyak@gmail.com>
2023-02-15 00:34:16 +0200

    React First Lesson

--- lesson-1-react-intro/task2/index.js
+++ lesson-1-react-intro/task2/index.js
@@ -1,3 +1,4 @@
+/*
 const rootElement = document.querySelector('#root');
 
 const renderGreeting = (elem) => {
@@ -18,3 +19,19 @@ const renderGreeting = (elem) => {
 };
 
 renderGreeting(rootElement);
+*/
+
+const rootElement = document.querySelector('#root');
+
+const element = React.createElement(
+  'div',
+  { className: 'greeting' },
+  React.createElement('div', { className: 'greeting__title' }, 'Hello, world!'),
+  React.createElement(
+    'div',
+    { className: 'greeting__text' },
+    "I'm learning React"
+  )
+);
+
+ReactDOM.render(element, rootElement);
