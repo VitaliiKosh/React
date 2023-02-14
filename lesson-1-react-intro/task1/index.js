5fc6cccec8cb375c4ef35f267e3d010d7afe5790
Vitaly Koshlyak <vitaly.koshlyak@gmail.com>
2023-02-15 00:21:35 +0200

    React First Lesson

--- lesson-1-react-intro/task1/index.js
+++ lesson-1-react-intro/task1/index.js
@@ -1,3 +1,12 @@
 /* eslint-disable no-undef */
 
 // put your code here
+const rootElement = document.querySelector('#root');
+
+const greetingElem = React.createElement(
+  'div',
+  { className: 'greeting' },
+  'Hello, React!'
+);
+
+ReactDOM.render(greetingElem, rootElement);
