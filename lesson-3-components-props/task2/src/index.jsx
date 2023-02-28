3bcdeb24f8ef1a98e4943eaf1e86906bfd679ce1
Vitaly Koshlyak <vitaly.koshlyak@gmail.com>
2023-02-28 15:44:33 +0200

    React Third Lesson

---
+++ lesson-3-components-props/task2/src/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.scss';
+import './search.scss';
+import Search from './Search';
+
+const rootElement = document.querySelector('#root');
+
+<Search />;
+
+ReactDOM.render(<Search name='Bob' age={17} />, rootElement);
